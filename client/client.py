f359dd6256b66e847140943a8fb236a7eb939834
iroan <2654189525@qq.com>
2018-01-20 22:59:30 +0800

    改用UDP协议通信

--- client/client.py
+++ client/client.py
@@ -17,28 +17,30 @@ Example:
 '''
 
 from docopt import docopt
-from twisted.internet import protocol,reactor
+from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import reactor
 from time import ctime
 import os
 
-def Twisted(ip, port):
-    from twisted.internet import protocol,reactor
-    class TSClntProtocol(protocol.Protocol):
-        def sendData(self):
-            data = input('>>>')
-            if data:
-                self.transport.write(data.encode())
-            else:
-                self.transport.lostConnection()
-        def connectionMade(self):
-            self.sendData()
-        def dataReceived(self,data):
-            print(data.decode())
-            self.sendData()
-    class TSClntFactory(protocol.ClientFactory):
-        protocol = TSClntProtocol
-        clientConnectionLost = clientConnectionFailed =lambda self,connector,reason:reactor.stop()
-    reactor.connectTCP(ip, port, TSClntFactory())
+class UDPProtocol(DatagramProtocol):
+    def __init__(self,ip,port):
+        self.ip = ip
+        self.port = port
+
+    def startProtocol(self):
+        self.transport.connect(self.ip,int(self.port))
+        self.sendDatagram(b'this is test by wkx')
+
+    def sendDatagram(self,string):
+        if len(string):
+            self.transport.write(string)
+        else:
+            reactor.stop()
+
+    def datagramReceived(self, datagram, addr):
+        print('Datagram recived:',datagram)
+def main(ip, port):
+    reactor.listenUDP(0,UDPProtocol(ip,port))
     reactor.run()
 
 if __name__ == '__main__':
@@ -49,4 +51,4 @@ if __name__ == '__main__':
         port = '11000'
     if not ip:
         ip = '127.0.0.1'
-    Twisted(ip,int(port))
+    main(ip, int(port))
--- server/server.py
+++ server/server.py
@@ -18,28 +18,19 @@ Example:
 # date:   2018/1/17
 
 from docopt import docopt
-from twisted.internet import protocol,reactor
+from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import reactor
 from time import ctime
 import os
 
 
+class UDPProtocol(DatagramProtocol):
+    def datagramReceived(self, datagram, addr):
+        self.transport.write(datagram,addr)
+
 def main(port):
-    class TSServProtocol(protocol.Protocol):
-        def sendData(self):
-            data = input('>>>')
-            if data:
-                self.transport.write(data.encode())
-            else:
-                self.transport.lostConnection()
-        def connectionMade(self): #当一个客户端连接上该服务器程序时执行
-            pass
-        def dataReceived(self, data):#当服务器接收到客户端发送的数据时执行
-            print(data.decode())
-            self.sendData()
-    factory = protocol.Factory()#返回一个实例
-    factory.protocol = TSServProtocol #新建的一个‘协议’
     print('waiting for connection and listen port:',port)
-    reactor.listenTCP(int(port),factory)
+    reactor.listenUDP(int(port),UDPProtocol())
     reactor.run()
 
 if __name__ == '__main__':
